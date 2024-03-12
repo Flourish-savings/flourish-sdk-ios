1708e0c0f9abb5eb609982c65a097b542906dd74
Rafael Kerr <rafael@flourishfi.com>
2024-03-12 16:18:07 -0300

    adding new events

---
+++ Sources/FlourishSDK/Events.swift
@@ -0,0 +1,20 @@
+import Combine
+
+public protocol EventListener: AnyObject {
+    func didReceiveEvent(data: Any)
+}
+
+@available(macOS 13.0, *)
+public class EventGenerator {
+    public let eventPublisher = PassthroughSubject<Any, Never>()
+
+    func subscribe(listener: EventListener) -> AnyCancellable {
+        return eventPublisher.sink { data in
+            listener.didReceiveEvent(data: data)
+        }
+    }
+
+    public func generateEvent(data: Any) {
+        eventPublisher.send(data)
+    }
+}
