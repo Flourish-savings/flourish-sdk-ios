e02b1b8cf6d3a49bb91a46ed95ba00eeac0e6b3f
Rafael Kerr <rafael@flourishfi.com>
2024-03-12 19:53:11 -0300

    delete unused code

--- Sources/FlourishSDK/Events.swift
+++ Sources/FlourishSDK/Events.swift
@@ -6,21 +6,13 @@ public protocol EventListener: AnyObject {
 
 @available(macOS 13.0, *)
 public class EventGenerator {
-    //public let eventPublisher = PassthroughSubject<Any, Never>()
     public let eventListener: EventListener
     
     public init(eventListener: EventListener) {
         self.eventListener = eventListener
     }
 
-//    public func subscribe(listener: EventListener) -> AnyCancellable {
-//        return eventPublisher.sink { data in
-//            listener.didReceiveEvent(data: data)
-//        }
-//    }
-
     public func generateEvent(data: Any) {
-        //eventPublisher.send(data)
         self.eventListener.didReceiveEvent(data: data)
     }
 }
