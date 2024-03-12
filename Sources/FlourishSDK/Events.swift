6e141c878943108019d785c162f2c28de90a4054
Rafael Kerr <rafael@flourishfi.com>
2024-03-12 18:51:31 -0300

    change event receiver

--- Sources/FlourishSDK/Events.swift
+++ Sources/FlourishSDK/Events.swift
@@ -6,19 +6,21 @@ public protocol EventListener: AnyObject {
 
 @available(macOS 13.0, *)
 public class EventGenerator {
-    public let eventPublisher = PassthroughSubject<Any, Never>()
+    //public let eventPublisher = PassthroughSubject<Any, Never>()
+    public let eventListener: EventListener
     
-    public init() {
-        
+    public init(eventListener: EventListener) {
+        self.eventListener = eventListener
     }
 
-    public func subscribe(listener: EventListener) -> AnyCancellable {
-        return eventPublisher.sink { data in
-            listener.didReceiveEvent(data: data)
-        }
-    }
+//    public func subscribe(listener: EventListener) -> AnyCancellable {
+//        return eventPublisher.sink { data in
+//            listener.didReceiveEvent(data: data)
+//        }
+//    }
 
     public func generateEvent(data: Any) {
-        eventPublisher.send(data)
+        //eventPublisher.send(data)
+        self.eventListener.didReceiveEvent(data: data)
     }
 }
